338f4f057e8d529a3fc7e7167d2bc5aa1e73c17f
realmarv <zahratehraninasab@gmail.com>
2023-06-01 12:59:25 +0330

    WIP2

--- bot/start.js
+++ bot/start.js
@@ -109,40 +109,44 @@ const askForConfirmation = async (user, command) => {
   }
 };
 
-const { SocksProxyAgent } = require('socks-proxy-agent');
 const mongoConnect = require('../db_connect');
-const { resubscribeInvoices } = require('../ln');
-const { delay } = require('../util');
-(async () => {
-  process.on('unhandledRejection', e => {
-    logger.error(`Unhandled Rejection: ${e.message}`);
-  });
+const mongoose = mongoConnect();
+mongoose()
 
-  process.on('uncaughtException', e => {
-    logger.error(`Uncaught Exception: ${e.message}`);
-  });
+// const { SocksProxyAgent } = require('socks-proxy-agent');
+// const mongoConnect = require('../db_connect');
+// const { resubscribeInvoices } = require('../ln');
+// const { delay } = require('../util');
+// (async () => {
+//   process.on('unhandledRejection', e => {
+//     logger.error(`Unhandled Rejection: ${e.message}`);
+//   });
 
-  const mongoose = mongoConnect();
-  mongoose.connection
-    .once('open', async () => {
-      logger.info('Connected to Mongo instance.');
-      let options = { handlerTimeout: 60000 };
+//   process.on('uncaughtException', e => {
+//     logger.error(`Uncaught Exception: ${e.message}`);
+//   });
 
-      if (process.env.SOCKS_PROXY_HOST) {
-        const agent = new SocksProxyAgent(process.env.SOCKS_PROXY_HOST);
-        options = {
-          telegram: {
-            agent,
-          },
-        };
-      }
-      const bot = start(process.env.BOT_TOKEN, options);
-      // Wait 1 seconds before try to resubscribe hold invoices
-      await delay(1000);
-      await resubscribeInvoices(bot);
-    })
-    .on('error', error => logger.error(`Error connecting to Mongo: ${error}`));
-})();
+//   const mongoose = mongoConnect();
+//   mongoose.connection
+//     .once('open', async () => {
+//       logger.info('Connected to Mongo instance.');
+//       let options = { handlerTimeout: 60000 };
+
+//       if (process.env.SOCKS_PROXY_HOST) {
+//         const agent = new SocksProxyAgent(process.env.SOCKS_PROXY_HOST);
+//         options = {
+//           telegram: {
+//             agent,
+//           },
+//         };
+//       }
+//       const bot = start(process.env.BOT_TOKEN, options);
+//       // Wait 1 seconds before try to resubscribe hold invoices
+//       await delay(1000);
+//       await resubscribeInvoices(bot);
+//     })
+//     .on('error', error => logger.error(`Error connecting to Mongo: ${error}`));
+// })();
 
 const initialize = (botToken, options) => {
   const i18n = new I18n({
