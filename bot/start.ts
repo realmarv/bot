0ac7e59b0f222aec08ca80912d007c10f9e522b0
realmarv <zahratehraninasab@gmail.com>
2023-06-08 18:29:16 +0430

    WIP34

--- bot/start.ts
+++ bot/start.ts
@@ -173,7 +173,7 @@ const initialize = (botToken, options) => {
     }
   });
 
-  bot.command('maintenance', superAdminMiddleware, async (ctx): Promise<string> => {
+  bot.command('maintenance', superAdminMiddleware, async (ctx): Promise<void> => {
     try {
       const [val] = await validateParams(ctx, 2, '\\<_on/off_\\>');
       if (!val) return;
